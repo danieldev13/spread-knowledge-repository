82c9eefd26bdc09e3f16f58377527e3efe116a58
Daniel Torres <daniel.ttorres@outlook.com>
2016-11-04 21:08:49 -0200

    Adding script
    Adding calculation script to find largest prime factor

---
+++ prime_factor/prime_maths.py
@@ -0,0 +1,13 @@
+def max_prime_factors(n):
+    """Returns all the prime factors of a positive integer"""
+    factors = []
+    d = 2
+    while n > 1:
+        while n % d == 0:
+            factors.append(d)
+            n /= d
+        d += 1
+        if d*d > n:
+            if n > 1: factors.append(n)
+            break
+    return max(factors)
